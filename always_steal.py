d9ba0f36e6f47217a239d5181322b0dc22940d2d
CoderElectronics <aristehney@gmail.com>
2024-05-13 15:51:40 -0400

    Add some demos

--- always_steal.py
+++ always_steal.py
@@ -1,16 +1,14 @@
 """
-Tit for tat game theory strategy for GameTheoryUI.
+Always steal game theory strategy for GameTheoryUI.
 by: Ari Stehney
 
 Customize the class below with your metadata and fill out the play function
 """
-from game_class import GameMove
-
 
 class ImportedStrat(GameStrategy):
     def __init__(self) -> None:
         # This is where your metadata will go:
-        super().__init__(name="Tit for Tat", author="Ari S.", description="This one is the winner")
+        super().__init__(name="Stealing Strategy", author="Ari S.", description="Always steals, no matter what")
 
     def next_play(self, player_history: list[GameMove], opponent_history: list[GameMove]) -> GameMove:
         """
@@ -18,13 +16,8 @@ class ImportedStrat(GameStrategy):
         :param opponent_history: List of the opponent's moves
         :return: Your next move
         """
-        # This is where your strategy logic goes
-        # Each item in the history arrays will be equal to GameMove.STEAL or GameMove.SHARE
 
-        if len(opponent_history) < 1:
-            return GameMove.SHARE
-        else:
-            return opponent_history[-1]
+        return GameMove.STEAL
 
 # This line is required!
 userGame = ImportedStrat()
--- very_random.py
+++ very_random.py
@@ -1,16 +1,15 @@
 """
-Tit for tat game theory strategy for GameTheoryUI.
+Random game theory strategy for GameTheoryUI.
 by: Ari Stehney
 
 Customize the class below with your metadata and fill out the play function
 """
-from game_class import GameMove
-
+import random
 
 class ImportedStrat(GameStrategy):
     def __init__(self) -> None:
         # This is where your metadata will go:
-        super().__init__(name="Tit for Tat", author="Ari S.", description="This one is the winner")
+        super().__init__(name="Randomized Strategy", author="Ari S.", description="Will give random plays each time")
 
     def next_play(self, player_history: list[GameMove], opponent_history: list[GameMove]) -> GameMove:
         """
@@ -20,11 +19,9 @@ class ImportedStrat(GameStrategy):
         """
         # This is where your strategy logic goes
         # Each item in the history arrays will be equal to GameMove.STEAL or GameMove.SHARE
+        global random
 
-        if len(opponent_history) < 1:
-            return GameMove.SHARE
-        else:
-            return opponent_history[-1]
+        return random.choice([GameMove.STEAL, GameMove.SHARE])
 
 # This line is required!
 userGame = ImportedStrat()
