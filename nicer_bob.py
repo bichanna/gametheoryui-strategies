ab377f0b8d18ec58508b929a998013635d620130
bichanna <nobu.bichanna@gmail.com>
2024-05-14 15:14:55 -0400

    Fixed some of Nobu's strategies

--- nicer_bob.py
+++ nicer_bob.py
@@ -1,13 +1,17 @@
 from game_class import GameStrategy, GameMove
 
-from functools import reduce
 import random
+
 
 class ImportedStrat(GameStrategy):
     def __init__(self) -> None:
-        super().__init__(name="Nicer Bob", author="Nobu", description="too lazy to explain")
+        super().__init__(
+            name="Nicer Bob", author="Nobu", description="too lazy to explain"
+        )
 
-    def next_play(self, player_history: list[GameMove], opponent_history: list[GameMove]) -> GameMove:
+    def next_play(
+        self, player_history: list[GameMove], opponent_history: list[GameMove]
+    ) -> GameMove:
         global random
 
         """
@@ -16,10 +20,12 @@ class ImportedStrat(GameStrategy):
         :return: Your next move
         """
 
-        share_weight = sum(opponent_history)
+        share_weight = sum([i.value for i in opponent_history])
         steal_weight = len(opponent_history) - share_weight
 
-        return random.choices([GameMove.SHARE, GameMove.STEAL], weights=(steal_weight, share_weight), k=1)[0]
+        return random.choices(
+            [GameMove.SHARE, GameMove.STEAL], weights=(share_weight + 1, steal_weight)
+        )[0]
 
 
 # This line is required!
