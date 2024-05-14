987db17e40d0ec58606a24cf55c558980300c30a
bichanna <nobu.bichanna@gmail.com>
2024-05-14 15:36:04 -0400

    Fix StStSh

--- ststsh.py
+++ ststsh.py
@@ -1,7 +1,7 @@
 class ImportedStrat(GameStrategy):
     def __init__(self) -> None:
         super().__init__(name="Timid Arthur", author="Nobu", description="")
-        self.n = 0
+        self.n = -1
 
     def next_play(
         self, player_history: list[GameMove], opponent_history: list[GameMove]
@@ -11,6 +11,7 @@ class ImportedStrat(GameStrategy):
         :param opponent_history: List of the opponent's moves
         :return: Your next move
         """
+        self.n += 1
         return GameMove.SHARE if self.n % 3 == 0 else GameMove.STEAL
 
 
