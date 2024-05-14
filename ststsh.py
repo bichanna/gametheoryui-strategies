2bac73a9a580ba9727beb5371ece427185e85f51
bichanna <nobu.bichanna@gmail.com>
2024-05-14 15:32:55 -0400

    Add StStSh

--- ststsh.py
+++ ststsh.py
@@ -1,9 +1,7 @@
-import random
-
-
 class ImportedStrat(GameStrategy):
     def __init__(self) -> None:
         super().__init__(name="Timid Arthur", author="Nobu", description="")
+        self.n = 0
 
     def next_play(
         self, player_history: list[GameMove], opponent_history: list[GameMove]
@@ -13,17 +11,7 @@ class ImportedStrat(GameStrategy):
         :param opponent_history: List of the opponent's moves
         :return: Your next move
         """
-        global random
-
-        his = len(opponent_history)
-        if his == 0:
-            return GameMove.SHARE
-
-        return (
-            GameMove.STEAL
-            if (sum([i.value for i in opponent_history]) / his) < 0.5
-            else random.choices([GameMove.SHARE, GameMove.STEAL], (3, 1))
-        )
+        return GameMove.SHARE if self.n % 3 == 0 else GameMove.STEAL
 
 
 # This line is required!
